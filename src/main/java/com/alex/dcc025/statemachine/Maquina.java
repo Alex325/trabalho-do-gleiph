d0fd660cd2c180d643a0623fc7aa25808f3e4a8b
Alex Sandro <alexsandromacedopinto@gmail.com>
2025-06-08 14:15:32 -0300

    Agora é um singleton

--- src/main/java/com/alex/dcc025/statemachine/Maquina.java
+++ src/main/java/com/alex/dcc025/statemachine/Maquina.java
@@ -1,19 +1,40 @@
 package com.alex.dcc025.statemachine;
 
 public class Maquina {
+
+    private static Maquina maquina;
+    
     private boolean fimDeJogo;
     private Estado estadoAtual;
 
-    public Maquina() {
-        estadoAtual = new EstadoFimDeJogo();        
+    public static Maquina maquina() {
+        if (maquina == null)
+            maquina = new Maquina();
+        
+        return maquina;
     }
-
+     
     public void run() {
         estadoAtual.run();
     }
 
-    public void transition(Estado novoEstado) {
+    public boolean jogoAcabou() {
+        return fimDeJogo;
+    }
+    
+    //nível de pacote, acessível para todos os estados
+    void transition(Estado novoEstado) {
         estadoAtual = novoEstado;
         run();
     }
+    
+    //nível de pacote, acessível para todos os estados
+    void setFimDeJogo(boolean acabou) {
+        fimDeJogo = acabou;
+    }
+    
+    private Maquina() {
+        estadoAtual = new EstadoTelaInicial();
+    }
+
 }
